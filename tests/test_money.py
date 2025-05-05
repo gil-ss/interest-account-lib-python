845bdeb78d83e01e191f0c6f8fa20e786ab3cdc6
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-05 11:39:35 +0100

    test: Add failing test for float input in to_money.

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,3 +1,7 @@
+from interest_account.money import to_money
+import pytest
+
+
 def test_should_convert_string_to_decimal():
     # We want to be able to convert "10.50" into a money-safe decimal value
     from interest_account.money import to_money
@@ -5,3 +9,8 @@ def test_should_convert_string_to_decimal():
     result = to_money("10.50")
 
     assert str(result) == "10.50"
+
+
+def test_to_money_should_raise_type_error_for_float():
+    with pytest.raises(TypeError):
+        to_money(0.01)
