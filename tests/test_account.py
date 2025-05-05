813089cd70bd0c90d579f19dd1f721afd508f983
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-05 13:34:49 +0100

    test: Ensure new InterestAccount starts with zero balance.

--- tests/test_account.py
+++ tests/test_account.py
@@ -6,3 +6,8 @@ def test_should_create_account_with_user_id_and_interest_rate():
     account = InterestAccount(user_id=user_id, interest_rate=0.5)
     assert account.user_id == user_id
     assert account.interest_rate == 0.5
+
+
+def test_new_account_should_start_with_zero_balance():
+    account = InterestAccount(user_id=uuid4(), interest_rate=0.5)
+    assert account.balance == Decimal("0.00")
