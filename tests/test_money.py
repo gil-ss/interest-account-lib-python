2f63e2cb19e4e5303a6bc6afcc51d5aca82c2e03
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-05 11:33:34 +0100

    Test: Add first failing test for converting string to decimal.

---
+++ tests/test_money.py
@@ -0,0 +1,7 @@
+def test_should_convert_string_to_decimal():
+    # We want to be able to convert "10.50" into a money-safe decimal value
+    from interest_account.money import to_money
+
+    result = to_money("10.50")
+
+    assert str(result) == "10.50"
