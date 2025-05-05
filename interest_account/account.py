07ca4c876bd99e6537bff80e4f797a69df333454
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-05 13:37:21 +0100

    feat: Initialize InterestAccount with zero balance.

--- interest_account/account.py
+++ interest_account/account.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
+
 class InterestAccount:
     def __init__(self, user_id, interest_rate):
         self.user_id = user_id
         self.interest_rate = interest_rate
+        self.balance = Decimal("0.00")
