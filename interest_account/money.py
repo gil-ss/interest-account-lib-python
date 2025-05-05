df8dfd2c251233916de9fafd450bd296dbf32a8d
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-05 11:46:54 +0100

    feat: Ensure to_money always returns Decimal with two decimal places.

--- interest_account/money.py
+++ interest_account/money.py
@@ -1,8 +1,9 @@
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
 
 
 def to_money(value: str | int | Decimal) -> Decimal:
     if isinstance(value, float):
         raise TypeError("Avoid using float. Use string, int, or Decimal to represent money.")
 
-    return Decimal(value)
+    decimal_value = Decimal(value)
+    return decimal_value.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
