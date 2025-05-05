b3120b8d93b4297adf5a0732bed5badb63d12f15
Gilberto Silveira <gilbertosilveira@192.168.1.103>
2025-05-05 13:40:10 +0100

    test: Ensure new InterestAccount starts with empty transaction list.

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from interest_account.account import InterestAccount
+from decimal import Decimal
 
 def test_should_create_account_with_user_id_and_interest_rate():
     user_id = uuid4()
@@ -11,3 +12,9 @@ def test_should_create_account_with_user_id_and_interest_rate():
 def test_new_account_should_start_with_zero_balance():
     account = InterestAccount(user_id=uuid4(), interest_rate=0.5)
     assert account.balance == Decimal("0.00")
+
+
+def test_new_account_should_start_with_empty_transaction_list():
+    account = InterestAccount(user_id=uuid4(), interest_rate=0.5)
+    assert isinstance(account.transactions, list)
+    assert len(account.transactions) == 0
